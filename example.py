1256d273a799d4d0f478bf37582c4a3b9e2987c6
hreikin <hreikin@gmail.com>
2025-02-07 17:38:07 +0000

    Enhance logging configuration in example.py: set up file and console handlers, and update log format

--- example.py
+++ example.py
@@ -1,13 +1,21 @@
-import p2pool_api, logging
+import logging
+from p2pool import P2PoolAPI
 
-logging.basicConfig()
-logging.getLogger("P2PoolAPI").setLevel(logging.INFO)           # Change to DEBUG to print out all responses when their methods are called
+# Configure the root logger
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level for the entire application, change to DEBUG to print all responses.
+    format='[%(asctime)s - %(name)s] - %(levelname)s - %(message)s',  # Consistent format
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file
+        logging.StreamHandler()  # Log to console
+    ]
+)
 log = logging.getLogger("MyLOG")
 
-api_path = "/path/to/p2pool/api"
-x = p2pool_api.P2PoolAPI(api_path)
+api_path = "api/"
+x = P2PoolAPI(api_path)
 
-log.info(x._local_stratum)                                      # Log entire reponse
-log.info(x.local_p2p_uptime)                                    # Log property representing individual data from the API
 x.get_stats_mod()                                               # Update individual `stats_mod` endpoint
 x.get_all_data()                                                # Update all endpoints at once
+log.info(x._local_stratum)                                      # Log entire response
+log.info(x.local_p2p_uptime)                                    # Log property representing individual data from the API
