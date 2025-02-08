f147bd3cb96f6a56e57651406fcbaf2556dd195c
hreikin <hreikin@gmail.com>
2025-02-08 12:56:04 +0000

    Initialize database connection in P2PoolAPI constructor

--- p2pool/api.py
+++ p2pool/api.py
@@ -52,6 +52,7 @@ class P2PoolAPI:
         self._pool_blocks_cache = []
         self._pool_stats_cache = {}
         self._stats_mod_cache = {}
+        P2PoolDatabase._init_db(self._db_url)
         self.update_all_endpoints()
 
     def _validate_api_path(self, api_path: str, is_remote: bool) -> bool:
