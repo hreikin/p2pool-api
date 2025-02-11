824a04384c0b67bd21a3134901ea289265aaf5b5
hreikin <hreikin@gmail.com>
2025-02-11 10:50:37 +0000

    Add logging configuration in env.py

---
+++ examples/env.py
@@ -0,0 +1,16 @@
+import logging
+# Configure the root logger
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level for the entire application, change to DEBUG to print all responses.
+    format='[%(asctime)s - %(name)s] - %(levelname)s - %(message)s',  # Consistent format
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file
+        logging.StreamHandler()  # Log to console
+    ]
+)
+log = logging.getLogger("ExampleLog")
+api_path = "api/"               # Can also be a URL: api_path = "http://example.com/api/"
+
+log.info("###################################################################################################################################")
+log.info("## Please ensure you have a running XMRig instance to connect to and have updated the connection details within the env.py file. ##")
+log.info("###################################################################################################################################")
