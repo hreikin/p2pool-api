3d6db0b8195050865616eb3549ce59e73c6540a4
hreikin <hreikin@gmail.com>
2025-02-11 13:55:29 +0000

    Bump version to 0.1.6

--- p2pool/__init__.py
+++ p2pool/__init__.py
@@ -52,7 +52,7 @@ Helpers:
 __name__ = "p2pool"
 __author__ = "hreikin"
 __email__ = "hreikin@gmail.com"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __license__ = "MIT"
 __description__ = "This module provides objects to interact with the P2Pool API and store collected data in a database."
 __url__ = "https://hreikin.co.uk/p2pool-api"
